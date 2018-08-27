39c5496ad7a668987220b4a0a7bc331f50bbfed0
hx6007 <32677621+hx6007@users.noreply.github.com>
2018-08-27 19:45:39 +0800

    Update letsencrypt_renew.sh

--- letsencrypt_renew.sh
+++ letsencrypt_renew.sh
@@ -2,7 +2,7 @@
 /usr/local/nginx/sbin/nginx -s stop
 /usr/local/src/letsencrypt/letsencrypt-auto renew -nvv --standalone > /var/log/letsencrypt/renew.log 2>&1
 LE_STATUS=$?
-/opt/nginx/sbin/nginx
+/usr/local/nginx/sbin/nginx
 
 if [ "$LE_STATUS" != 0 ]; then
     echo Automated renewal failed:
